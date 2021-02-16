65331d03df6f7dfd3a1f56e0c2d40a050784cc4d
Taiyo <taiyospresence@gmail.com>
2021-02-16 17:51:37 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -5,17 +5,26 @@ $("#myBtn").click(function () {
   var inp = $(".card-main-number");
   var err = $(".error-msg");
   const inpVal = inp.val();
+  const margeInp = $(".marge-input");
   console.log(inpVal.length);
   if (inpVal.length > 12 && inpVal.length <= 19) {
     console.log("valid");
   } else {
-    inp.addClass("error");
+    margeInp.addClass("error");
     err.toggleClass("d-none");
   }
 });
 
-$(".card-main-number").change(function (e) {
-  console.log(e);
+$(".card-wrapper").click(function (e) {
+  const selectedCardNumber = $(".card-number-para").text().split(" ").join("");
+  selectedCardNumber;
+  $("#cardNumber").val(
+    selectedCardNumber
+      .replace(/[^\dA-Z]/g, "")
+      .replace(/(.{4})/g, "$1 ")
+      .trim()
+  );
+  $(".card-outer").toggleClass("d-none");
 });
 
 $(document).ready(function () {
@@ -37,4 +46,31 @@ $("input:radio").change(function () {
   } else {
     $(".one").show();
   }
+});
+function movetoNext(current, nextFieldID) {
+  if (current.value.length >= current.maxLength) {
+    document.getElementById(nextFieldID).focus();
+  }
+}
+
+$(function () {
+  $(".marge-input").on("click", () => {
+    $(".marge-input").css({
+      borderColor: "#86b7fe",
+      outline: 0,
+      boxShadow: "0 0 0 0.25rem rgb(13 110 253 / 25%)",
+    });
+  });
+  $(document).on("click", function (e) {
+    if ($(e.target).is(".no-border") === false) {
+      $(".marge-input").removeAttr("style");
+    }
+  });
+});
+
+$(function () {
+  $("body").click(function () {
+    if (!$(this.target).is("#marge-input")) {
+    }
+  });
 });
